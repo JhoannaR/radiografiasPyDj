4d5082ddced7668400eb9b585c49aad145367c97
Jhoanna Rosa Mera Dávila <jhoannarosameradavila@gmail.com>
2023-05-06 13:39:47 -0500

    generando archivo de dependencias

--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+# from myapp.models import MyModel
+#el import anterior es para mostrar los metadatos(nombre de las estructuras)
 import pydicom
 from pylinac import load_log
+import json
 
 
 from django.conf import settings
@@ -10,38 +12,59 @@ from pydicom.data import get_testdata_file
 import os
 import numpy as np
 
-# Create your views here.
+# -------------------------------------DICOM FUNCTION----------------------------------------
 def dicom(request):
     # Carga el archivo DICOM
     path = os.path.join(settings.MEDIA_ROOT, 'D0001.dcm')
     # filename=get_testdata_file(path)
     dcm = pydicom.dcmread(path)
 
+
     fg = dcm.group_dataset
     data = dcm.pixel_array
+    
     # print('The image has {} x {} voxels'.format(data.shape[0],
     #                                         data.shape[1]))
-    # structure=dcm.pixel_
-    # max_structure=
 
     # Convierte la matriz de dosis a un array de numpy
     dose_array = np.array(data)
 
     # Encuentra la dosis máxima en el array de dosis
     max_dosis = np.amax(dose_array)  
-    #mayor radiación punto mas oscurso
+    #mayor radiación punto mas oscurso mayor número
 
-    print(fg)
+    new_data=dcm.pixel_array.tolist()  
+    #array del final es el array del gráfico
+    # print(new_data)
+    ds_json=dcm.to_json()
+    # print(ds_json)
+    dicom_str = json.dumps(json.loads(ds_json), indent=4)
+    # ds_data=json.loads(dicom_str)
+    # ds_datatrans=ds_data.get('00080005')
+    # ds_datatrans_json=json.dumps(ds_datatrans)
+    print(dcm.get('00080005'))
+    # print(ds_datatrans_json[1])
+    # print(type(ds_json))
+    # print(type(dicom_str))
+    # print(fg)
+
     # Obtiene el nombre de las estructuras
     # estructuras=dcm.StructureSetROISequence
 
     variable="Jhoanna"
     context={'variable': variable,
               'dosismax': max_dosis,
-              'dose_array':dose_array}
+              'dose_array':dose_array,
+              'structures_name':fg,
+              'pixel_data':dicom_str,
+
+              }
     return render(request, 'dicom.html', context)
     # return HttpResponse(estructuras)
 
+
+
+# -------------------------------------DYNALOG FUNCTION----------------------------------------
 
 def dynalog(request):
     path = os.path.join(settings.MEDIA_ROOT, 'Tlog.bin')
@@ -66,5 +89,10 @@ def dynalog(request):
     tlog.fluence.gamma.plot_map()  # show the gamma map as a matplotlib figure
     # tlog.publish_pdf()  # publish a PDF report
     # dlog = load_log("dynalog.dlg")
-    return HttpResponse("dynalog")
 
+    # return HttpResponse("dynalog")
+    variable2="Dynalog"
+
+    context={'variable': variable2,
+            }
+    return render(request, 'dynalog.html', context)
